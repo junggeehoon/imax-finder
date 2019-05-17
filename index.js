8e20a310f8c1efcda8de17e4cdf79ac0e952f20e
junggeehoon <junggeehoon@gmail.com>
2019-05-17 11:49:31 -0700

    파일이름변경

--- index.js
+++ index.js
@@ -2,21 +2,20 @@ const puppeteer = require('puppeteer');
 const nodemailer = require('nodemailer');
 const config = require('./config');
 
-const regionCode = {
+const areaCode = {
   "서울": 1,
   "경기": 2,
   "인천": 3,
-  "부산": 4,
-  "울산": 5,
+  "강원": 4,
+  "대전": 5,
+  "충청": 5,
   "대구": 6,
-  "대전": 7,
-  "광주": 8,
-  "강원": 9,
-  "경남": 10,
-  "경북": 11,
-  "전라": 12,
-  "충청": 13,
-  "제주": 14
+  "부산": 7,
+  "울산": 7,
+  "경상": 8,
+  "광주": 9,
+  "전라": 9,
+  "제주": 9
 };
 
 const transporter = nodemailer.createTransport({
@@ -55,12 +54,13 @@ const visitHomepage = async () => {
     await dialog.dismiss();
   });
   const config = {
-    homepage: 'http://section.cgv.co.kr/theater/popup/r_TimeTable.aspx',
-    title: '어벤져스-엔드게임',
+    homepage: 'http://www.cgv.co.kr/reserve/show-times/',
+    title: ' 어벤져스: 엔드게임',
     region: '서울',
-    theater: 'CGV 압구정',
-    month: 05,
-    day: 18
+    theater: 'CGV용산아이파크몰',
+    year: '2019',
+    month: '05',
+    day: '24'
   }
   try {
     await page.setViewport({
@@ -69,73 +69,58 @@ const visitHomepage = async () => {
     });
     await page.goto(config.homepage);
     await page.waitFor(1000);
-    await page.click(`#UpdatePanel1 > ul > li.theater_tab_area > p > a:nth-child(${regionCode[config.region]})`);
-    await page.waitFor(1000);
 
+    await page.click(`#contents > div.sect-common > div > div.sect-city > ul > li:nth-child(${areaCode[config.region]})`); //지역선택
+
+    let nthElement = 1;
     let theater = await page.evaluate(() => {
-      return document.querySelector(`#divWrap > a:nth-child(${1}) > span`).innerHTML;
+      return document.querySelector(`#contents > div.sect-common > div > div.sect-city > ul > li.on > div > ul > li:nth-child(${1}) > a`).innerHTML;
     })
-
-    let theaterCode = 1;
 
     while (theater !== config.theater) {
-      theaterCode++;
-      theater = await page.evaluate(theaterCode => {
-        return document.querySelector(`#divWrap > a:nth-child(${theaterCode}) > span`).innerHTML;
-      }, theaterCode)
-    }
-    await page.click(`#divWrap > a:nth-child(${theaterCode}) > span`);
-
-    await page.waitFor(1000);
-    
-    let lastDayClass = await page.evaluate(() => {
-      return document.querySelector(`#TheaterTimeTable_UpdatePanel1 > div > div > ul`).lastElementChild.className;
-    })
-    let lastDay;
-    
-    let nthElement = 1;
-    let dateClass = await page.evaluate(() => {
-      return document.querySelector(`#TheaterTimeTable_UpdatePanel1 > div > div > ul > li:nth-child(${1})`).className;
-    })
-    let dayFlag = 0;
-
-    if (lastDayClass === 'die') {
-
-      while (dateClass !== 'die') {
-        nthElement++;
-        dateClass = await page.evaluate(nthElement => {
-          return document.querySelector(`#TheaterTimeTable_UpdatePanel1 > div > div > ul > li:nth-child(${nthElement})`).className;
-        }, nthElement)
-      }
-  
-      lastDay = await page.evaluate(nthElement => {
-        return document.querySelector(`#TheaterTimeTable_UpdatePanel1 > div > div > ul > li:nth-child(${nthElement - 1})`).lastElementChild.lastElementChild.innerHTML;
+      nthElement++;
+      theater = await page.evaluate(nthElement => {
+        return document.querySelector(`#contents > div.sect-common > div > div.sect-city > ul > li.on > div > ul > li:nth-child(${nthElement}) > a`).innerHTML;
       }, nthElement)
 
-    } else {
-      dayFlag = 1;
-      lastDay = await page.evaluate(() => {
-        return document.querySelector(`#TheaterTimeTable_UpdatePanel1 > div > div > ul`).lastElementChild.lastElementChild.lastElementChild.innerHTML;
+    }
+    const theaterCodeInner = await page.evaluate(nthElement => {
+      return document.querySelector(`#contents > div.sect-common > div > div.sect-city > ul > li.on > div > ul > li:nth-child(${nthElement})`).innerHTML;
+    }, nthElement)
+    const theaterCode = theaterCodeInner.split('&')[1].split('=')[1];
+    const url = `http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=0${areaCode[config.region]}&theatercode=${theaterCode}&date=${config.year}${config.month}${config.day}`;
+
+    await page.goto(url);
+
+    const day = await page.evaluate(() => {
+      return document.querySelector(`li.on > div > a > strong`).innerText;
+    })
+
+    let flag = false;
+    if (day === config.day) {
+      const moviesNumber = await page.evaluate(() => {
+        return document.querySelectorAll(`.info-movie`).length;
       })
+
+      for (let i = 1; i <= moviesNumber; i++) {
+        const movie = await page.evaluate(i => {
+          return document.querySelector(`body > div > div.sect-showtimes > ul > li:nth-child(${i}) > div > div:nth-child(1) > a > strong`).innerText;
+        }, i)
+        if (movie === config.title) {
+          flag = true;
+          break;
+        }
+      }
     }
 
-    const date = lastDay.split('.');
-    const month = Number(date[0]);
-    const day = Number(date[1]);
-    if (month === config.month && day >= config.day) {
-      if (dayFlag === 0) {
-        await page.click(`#TheaterTimeTable_UpdatePanel1 > div > div > ul > li:nth-child(${nthElement - 1})`);
-      } else {
-        await page.click(`#TheaterTimeTable_UpdatePanel1 > div > div > ul`).lastElementChild;
-      }
-      await page.waitFor(1000);
-
-      // const image = Date.now();
-      // await page.screenshot({path: `./screenshot/${image}.png`, fullPage: true});
+    if (flag) {
+      console.log("빨리 예매하세요!!");
+      const image = Date.now();
+      await page.screenshot({path: `./screenshot/${image}.png`, fullPage: true});
       // sendMail(image);
     }
 
-    // return browser.close();
+    return browser.close();
 
   } catch (err) {
     console.log(err);
